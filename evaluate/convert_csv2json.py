6212d161ee12c839f2ddb09f8de751774f03f565
hrzhao76 <hrzhao76@gmail.com>
2023-10-06 02:17:23 -0700

    more events to a json

--- evaluate/convert_csv2json.py
+++ evaluate/convert_csv2json.py
@@ -10,26 +10,73 @@ import pandas as pd
 import numpy as np 
 import json 
 
-input_file = Path("../exatrkx_pipeline/datanmodels/in_e1000.csv")
+import argparse
+from typing import Union
+from tqdm import tqdm
 
-# 1. Read the CSV file into a pandas DataFrame
-df = pd.read_csv(input_file, names=['x', 'y', 'z'])
+def read_csv(input_file: Union[str, Path]) -> pd.DataFrame:
+    """Read the CSV file into a pandas DataFrame.
 
-# 2. Convert the DataFrame to a flattened list
-flattened_list = df.values.flatten().tolist()
+    Args:
+        input_file (Union[str, Path]): Path to the CSV file.
 
-# 3. Create a dictionary with the desired structure
-data_structure = {
-    "data": [
-        {
-            "FEATURES": {
-                "content": flattened_list,
-                "shape": list(df.shape)
-            }
-        }
-    ]
-}
+    Returns:
+        pd.DataFrame: DataFrame containing the CSV data.
+    """
+    input_file_path = Path(input_file) if isinstance(input_file, str) else input_file
 
-with open(input_file.parent / f'{input_file.stem}.json', 'w') as json_file:
-    json.dump(data_structure, json_file, indent=4)
+    if not input_file_path.exists():
+        raise FileNotFoundError(f"{input_file_path} does not exist.")
 
+    df = pd.read_csv(input_file_path, names=['x', 'y', 'z'])
+
+    return df
+
+def csv2json(user_input: Union[str, Path]) -> dict:
+    """Convert the CSV file/files to a JSON file.
+
+    Args:
+        user_input (Union[str, Path]): user input, can be a csv file or a folder containing CSV files.
+
+    Raises:
+        FileNotFoundError: if the input file does not exist, raise FileNotFoundError.
+        ValueError: if the input is not a file or folder, raise ValueError.
+
+    Returns:
+        dict: a dictionary containing the data.
+    """
+    user_input_path = Path(user_input) if isinstance(user_input, str) else user_input 
+
+    if not user_input_path.exists():
+        raise FileNotFoundError(f"{user_input_path} does not exist.")
+    
+    output_data_structure = {"data" : []}
+    # check if the input is a file or folder 
+    if user_input_path.is_file():
+        df = read_csv(user_input_path)
+        flattened_list = df.values.flatten().tolist()
+        output_data_structure["data"].append({"FEATURES": {"content": flattened_list, "shape": list(df.shape)}})
+
+    elif user_input_path.is_dir():
+        for csv_file in tqdm(user_input_path.glob("*.csv")):
+            df = read_csv(csv_file)
+            flattened_list = df.values.flatten().tolist()
+            output_data_structure["data"].append({"FEATURES": {"content": flattened_list, "shape": list(df.shape)}})
+
+    else:
+        raise ValueError(f"{user_input_path} is not a file or folder.")
+
+    output_json = user_input_path.parent / f'{user_input_path.stem}.json'
+    with open(output_json, 'w') as json_file:
+        json.dump(output_data_structure, json_file, indent=4)
+    
+    return output_data_structure
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Convert CSV to JSON')
+    parser.add_argument('--input', type=str, help='Input CSV file, defalut is in_e1000.csv', default="/workspace/exatrkx_pipeline/datanmodels/in_e1000.csv")
+    args = parser.parse_args()
+    user_input = args.input
+
+    csv2json(user_input)
